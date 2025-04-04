61ef76a3ac02ac44a6e7a3c3b4f9f48c95a87538
Ritesh Raj Moond <moondritesh@gmail.com>
2025-04-04 13:21:30 +0530

    authService

--- AuthService/app/src/main/java/authservice/AuthServiceApplication.java
+++ AuthService/app/src/main/java/authservice/AuthServiceApplication.java
@@ -1,7 +1,15 @@
 package authservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "authservice.repository")
+@ComponentScan(basePackages = { "authService.controller", "authService.auth", "authService.service" })
 public class AuthServiceApplication {
     public static void main(String[] args) {
-        
+        SpringApplication.run(AuthServiceApplication.class, args);
     }
 }
